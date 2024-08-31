332dce6a5f33b07cc119f0c2564268797ec34fe7
marcelinombb <dsoftware66.cc@interconta.com.br>
2024-08-31 20:21:44 -0300

    feat(Recipes): criação do component de Tabs na telas de recipes.

--- src/pages/Recipes/index.tsx
+++ src/pages/Recipes/index.tsx
@@ -1,16 +1,14 @@
-/* eslint-disable @typescript-eslint/consistent-type-imports */
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useEffect, useState } from 'react'
 import NavBar from '../../components/common/NavBar'
-import { Container } from './styles'
+import { Container, ContainerTabRecipes, TabButton, TabButtonText, TabRecipes } from './styles'
 import DefaultTitle from '../../components/common/DefaultTitle'
 import SearchBar from 'src/components/common/SearchBar'
-import RecipeCard, { type RecipeCardProps } from 'src/components/RecipeCard'
-import { FlatList } from 'react-native'
-import { Recipe } from 'src/entitites/Recipe'
+import RecipeCard from 'src/components/RecipeCard'
+import { FlatList, Text, View } from 'react-native'
+import { type Recipe } from 'src/entitites/Recipe'
 import recipeService from 'src/services/recipeService'
 import { useNavigation } from '@react-navigation/native'
-import { PropsStack } from 'src/routes'
+import { type PropsStack } from 'src/routes'
 
 const Recipes = () => {
   const [recipes, setRecipes] = useState<Recipe[] | []>([])
@@ -47,17 +45,39 @@ const Recipes = () => {
       }}
     />
   )
+
+  const [tabActive, setTabActive] = useState<string>('recipes')
+
+  function isActive (name: string) {
+    return tabActive === name
+  }
+
   return (
     <Container>
       <DefaultTitle fontSize={20} title="Recipes" />
       <SearchBar />
-      <FlatList
+      <ContainerTabRecipes>
+        <TabRecipes>
+          <TabButton active={isActive('recipes')} onPress={() => { setTabActive('recipes') }}>
+            <TabButtonText active={isActive('recipes')}>Recipes</TabButtonText>
+          </TabButton>
+          <TabButton active={isActive('myrecipes')} onPress={() => { setTabActive('myrecipes') }}>
+            <TabButtonText active={isActive('myrecipes')}>My recipes</TabButtonText>
+          </TabButton>
+        </TabRecipes>
+      </ContainerTabRecipes>
+      {
+        isActive('recipes')
+          ? <FlatList
         data={recipes}
         keyExtractor={(item: Recipe) => item.id}
         renderItem={renderItem}
         showsVerticalScrollIndicator={false}
         contentContainerStyle={{ paddingBottom: 70 }}
       />
+          : <View><Text>Nenhuma receita.</Text></View>
+
+      }
       <NavBar />
     </Container>
   )
